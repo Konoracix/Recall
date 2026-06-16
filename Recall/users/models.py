42e9391895230c6387f6aaf92e8f6b31cff3c691
Radosław <radek.barylak@gmail.com>
2026-06-16 11:39:55 +0200

    Added user manager for custom user auth model

--- Recall/users/models.py
+++ Recall/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from users.managers import UserManager
 
 class TimeStampsModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,6 +15,8 @@ class User(AbstractUser, TimeStampsModel):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=50, blank=True)
     last_name = models.CharField(max_length=50, blank=True)
+
+    objects = UserManager()
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
